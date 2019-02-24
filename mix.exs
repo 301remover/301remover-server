02fc82113b7f3e1c9603f535233eda5a3106d0b9
Sean Lang <slang800@gmail.com>
2019-02-24 15:23:40 -0600

    sort deps

--- mix.exs
+++ mix.exs
@@ -32,10 +32,10 @@ defmodule Unshortener.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.0"},
-      {:phoenix_pubsub, "~> 1.1"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix, "~> 1.4.0"},
       {:plug_cowboy, "~> 2.0"}
     ]
   end
