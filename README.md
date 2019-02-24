1c13f66a7bba35eeb8a686c0fa775ce5c1a689b8
Sean Lang <slang800@gmail.com>
2019-02-24 15:22:03 -0600

    add project files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# server
+# 301remover-server
 
+Part of the 301remover project.
+
+This component is the server that takes requests from the browser extension. It handles them by delegating the requests to a cluster of RPC servers
---
+++ test/unshortener_web/views/error_view_test.exs
@@ -0,0 +1,15 @@
+defmodule UnshortenerWeb.ErrorViewTest do
+  use UnshortenerWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.json" do
+    assert render(UnshortenerWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500.json" do
+    assert render(UnshortenerWeb.ErrorView, "500.json", []) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
