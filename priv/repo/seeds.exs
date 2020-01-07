fbe9f390c9769831b9adfef8a4cae9c2799f25a8
Sean Lang <slang800@gmail.com>
2020-01-07 16:13:05 -0600

    use shorteners.json in seed script

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -3,18 +3,9 @@
 
 alias Unshortener.Shorteners
 
-{:ok, body} = File.read("urlteam.json")
+{:ok, body} = File.read("priv/repo/shorteners.json")
 json = Jason.decode!(body)
 
 Enum.each(json, fn service ->
-  name = Map.fetch!(service, "name")
-  shortcode_alphabet = Map.fetch!(service, "shortcode_alphabet")
-  url_pattern = Map.fetch!(service, "url_pattern")
-
-  Shorteners.create_shortener!(%{
-    "domain" => name,
-    "url_pattern" => url_pattern,
-    "shortcode_alphabet" => shortcode_alphabet,
-    "supports_https" => false
-  })
+  Shorteners.create_shortener!(service)
 end)
